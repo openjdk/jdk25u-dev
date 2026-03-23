69e2a9795045b4956a91d97fceb87c9d27d75a15
Mohamed Issa <mohamed.issa@intel.com>
2026-03-23 11:59:38 -0700

    Remove avx512_vbmi and bmi2 enums from IREncodingPrinter.java source file.

--- test/hotspot/jtreg/compiler/lib/ir_framework/test/IREncodingPrinter.java
+++ test/hotspot/jtreg/compiler/lib/ir_framework/test/IREncodingPrinter.java
@@ -104,9 +104,7 @@ public class IREncodingPrinter {
         "avx512f",
         "avx512_fp16",
         "avx512_vnni",
-        "avx512_vbmi",
         "avx10_2",
-        "bmi2",
         // AArch64
         "sha3",
         "asimd",
