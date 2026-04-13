7dac61a2bd28dfc7668cead9ca94cac9c1bb3434
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2026-04-13 12:52:22 +0200

    implement review

--- src/java.desktop/windows/classes/sun/awt/windows/WPrinterJob.java
+++ src/java.desktop/windows/classes/sun/awt/windows/WPrinterJob.java
@@ -1742,9 +1742,7 @@ public final class WPrinterJob extends RasterPrinterJob
                 attributes.add(new PageRanges(from, to));
                 setPageRange(from, to);
             } else {
-                // Sets default values for PageRange attribute and setPageRange
-                attributes.add(new PageRanges(1,
-                                              Integer.MAX_VALUE));
+                attributes.remove(PageRanges.class);
                 setPageRange(Pageable.UNKNOWN_NUMBER_OF_PAGES,
                              Pageable.UNKNOWN_NUMBER_OF_PAGES);
             }
